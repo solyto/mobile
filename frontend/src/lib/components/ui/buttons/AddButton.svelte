603024ad8b537ad622210182f7649b557d5d1164
Leander Muskalla <muskalla@sozialit.de>
2026-07-24 17:57:20 +0200

    chore: release v1.0.13

--- frontend/src/lib/components/ui/buttons/AddButton.svelte
+++ frontend/src/lib/components/ui/buttons/AddButton.svelte
@@ -8,17 +8,17 @@
 </script>
 
 {#if colorOnHover}
-	<div
+	<button
 		class="group relative z-40 flex size-[34px] cursor-pointer items-center justify-center rounded-full border-c-neutral-3 text-c-neutral-4 transition-all"
+		onclick={onClick}
 	>
-		<button
+		<div
 			class="absolute flex h-0 w-0 cursor-pointer items-center justify-center rounded-full bg-c-btn text-white transition-all group-hover:h-full group-hover:w-full hover:shadow-sm"
-			onclick={onClick}
 		>
 			<IconPlus class="size-5" />
-		</button>
+		</div>
 		<IconPlus class="size-5" />
-	</div>
+	</button>
 {:else}
 	<button
 		class="z-40 cursor-pointer rounded-full bg-c-btn p-[7px] text-white drop-shadow-md transition-all hover:text-c-heading hover:drop-shadow-xl"
